84ed71740d314599dd54a474550d1784a9789dec
Okke Trommelen <okke_trommelen@live.nl>
2020-09-24 21:52:59 +0200

    added dir light to camera

--- src/STLViewer.js
+++ src/STLViewer.js
@@ -42,18 +42,19 @@ class STLViewer extends Component {
         let xDims, yDims, zDims;
         let component = this;
 
-
+        camera = new THREE.PerspectiveCamera(30, width / height, 1, 10000);
         scene = new THREE.Scene();
-        distance = 10000;
+
         const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
         directionalLight.position.x = 0;
         directionalLight.position.y = 1;
         directionalLight.position.z = 0;
         directionalLight.position.normalize();
-        scene.add(directionalLight);
+        camera.add(directionalLight);
 
         const ambientLight = new THREE.AmbientLight(0x404040); // soft white light
         scene.add(ambientLight);
+        scene.add(camera);
 
         const onLoad = geometry => {
             geometry.computeFaceNormals();
